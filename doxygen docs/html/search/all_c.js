6a53bc01c5d6502ca5d4a981fb52c32859779eb5
iDarkAxe <idarkaxe@gmail.com>
2024-03-03 13:08:20 +0100

    doxygen corrections

--- doxygen docs/html/search/all_c.js
+++ doxygen docs/html/search/all_c.js
@@ -2,10 +2,9 @@ var searchData=
 [
   ['tetrimino_0',['TETRIMINO',['../struct_t_e_t_r_i_m_i_n_o.html',1,'']]],
   ['tetris_20game_1',['Tetris Game',['../index.html',1,'']]],
-  ['tetrisborduredecor_2',['tetrisBordureDecor',['../user_oled_8c.html#a1b46f95cb32d9af8eca89bad57d18210',1,'userOled.c']]],
-  ['tetrisgame_3',['tetrisGame',['../tetris_game_8c.html#aaf103d842e6d9f5524319094721d4268',1,'tetrisGame.c']]],
-  ['tetrisgame_2ec_4',['tetrisGame.c',['../tetris_game_8c.html',1,'']]],
-  ['tetrisinit_5',['tetrisInit',['../tetris_game_8c.html#ade063724957c75dd97f122a38e1ac92b',1,'tetrisGame.c']]],
-  ['tetrisleveling_2ec_6',['tetrisLeveling.c',['../tetris_leveling_8c.html',1,'']]],
-  ['tetrisscoring_2ec_7',['tetrisScoring.c',['../tetris_scoring_8c.html',1,'']]]
+  ['tetrisgame_2',['tetrisGame',['../tetris_game_8c.html#aaf103d842e6d9f5524319094721d4268',1,'tetrisGame.c']]],
+  ['tetrisgame_2ec_3',['tetrisGame.c',['../tetris_game_8c.html',1,'']]],
+  ['tetrisinit_4',['tetrisInit',['../tetris_game_8c.html#ade063724957c75dd97f122a38e1ac92b',1,'tetrisGame.c']]],
+  ['tetrisleveling_2ec_5',['tetrisLeveling.c',['../tetris_leveling_8c.html',1,'']]],
+  ['tetrisscoring_2ec_6',['tetrisScoring.c',['../tetris_scoring_8c.html',1,'']]]
 ];
--- doxygen docs/html/search/functions_c.js
+++ doxygen docs/html/search/functions_c.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['tetrisborduredecor_0',['tetrisBordureDecor',['../user_oled_8c.html#a1b46f95cb32d9af8eca89bad57d18210',1,'userOled.c']]],
-  ['tetrisgame_1',['tetrisGame',['../tetris_game_8c.html#aaf103d842e6d9f5524319094721d4268',1,'tetrisGame.c']]],
-  ['tetrisinit_2',['tetrisInit',['../tetris_game_8c.html#ade063724957c75dd97f122a38e1ac92b',1,'tetrisGame.c']]]
+  ['tetrisgame_0',['tetrisGame',['../tetris_game_8c.html#aaf103d842e6d9f5524319094721d4268',1,'tetrisGame.c']]],
+  ['tetrisinit_1',['tetrisInit',['../tetris_game_8c.html#ade063724957c75dd97f122a38e1ac92b',1,'tetrisGame.c']]]
 ];
